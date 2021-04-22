f042fdfd24a6c7462a759d77557068d4e53f712f
ayansome1 <ayansome1@gmail.com>
2021-04-22 19:15:13 +0530

    display manager address

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import web3 from './web3';
+import React, { useState, useEffect } from 'react';
+
+// import web3 from './web3';
+import lottery from './lottery';
 
 function App() {
-  web3.eth.requestAccounts().then((v) => console.log(v));
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [manager, setManager] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const manager = await lottery.methods.manager().call();
+      setManager(manager);
+    })();
+  }, []);
+
+  return <div className='App'>{manager}</div>;
 }
 
 export default App;
